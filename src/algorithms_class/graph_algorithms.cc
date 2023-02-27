bdac11b5742fb271c9465341ebc66084284d7419
rubiezzy <rubiezzy@student.21-school.ru>
2023-02-27 22:26:36 +0400

    added test case for dfs

--- src/algorithms_class/graph_algorithms.cc
+++ src/algorithms_class/graph_algorithms.cc
@@ -1,2 +1,9 @@
 #include "graph_algorithms.h"
+#include "../helpers/containers/s21_stack.h"
 
+std::vector<int> s21::GraphAlgorithms::depthFirstSearch(Graph &graph, int startVertex) {
+  std::vector<int> visited_nodes;
+  s21::stack<int> not_visited_nodes;
+  
+
+}

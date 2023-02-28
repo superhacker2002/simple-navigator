1573828c52b594534f1ea116b51f9cb81ec90091
rubiezzy <rubiezzy@student.21-school.ru>
2023-02-28 19:11:49 +0400

    tests for iterator

--- tests/graph_tests.cc
+++ tests/graph_tests.cc
@@ -3,14 +3,32 @@
 
 #include <gtest/gtest.h>
 #include <vector>
+#include <iostream>
 
-const std::vector<int> answer = {0, 29, 20, 21, 16, 31, 100, 12, 4, 31, 18};
+const std::vector<int> answer = {0, 12, 0, 0, 0,
+                                 0, 0, 0, 20, 8, 
+                                 11, 0, 0, 17, 0, 
+                                 0, 0, 0, 0, 0,
+                                 0, 0, 0, 0, 0};
 
-TEST(graph_tests, iterator) {
+TEST(graph_tests, iterator_operator_plus) {
   s21::Graph graph;
-  graph.loadGraphFromFile("datasets/sample1.txt");
+  graph.loadGraphFromFile("../datasets/sample2.txt");
+  auto answer_it = answer.begin();
   for (auto it = graph.begin(); it != graph.end(); ++it) {
-    
+    EXPECT_EQ(*it, *answer_it);
+    answer_it++;
+  }
+}
+
+TEST(graph_tests, iterator_operator_minus) {
+  s21::Graph graph;
+  graph.loadGraphFromFile("../datasets/sample2.txt");
+  auto answer_it = answer.end();
+  answer_it--;
+  for (auto it = graph.end(); it != graph.begin(); --it) {
+    EXPECT_EQ(*it, *answer_it);
+    answer_it--;
   }
 }
 

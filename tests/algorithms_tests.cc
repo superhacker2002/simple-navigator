ca48ee416864fe7b4756efe525af55e8b53cb39c
rubiezzy <rubiezzy@student.21-school.ru>
2023-03-01 21:13:48 +0400

    test cases for shortest path

--- tests/algorithms_tests.cc
+++ tests/algorithms_tests.cc
@@ -5,7 +5,7 @@
 const std::vector<int> bfs_1_answer = {1, 2, 3, 5, 4, 6, 7};
 const std::vector<int> dfs_1_answer = {1, 3, 6, 7, 4, 2, 5};
 
-struct AlgorithmsTest : public testing::Test {
+struct SearchAlgorithmsTest : public testing::Test {
  protected:
   s21::Graph graph;
   void SetUp() {
@@ -14,7 +14,7 @@ struct AlgorithmsTest : public testing::Test {
   void TearDown() {}
 };
 
-TEST_F(AlgorithmsTest, dfs_1) {
+TEST_F(SearchAlgorithmsTest, dfs_1) {
   std::vector<int> result = s21::GraphAlgorithms::depthFirstSearch(graph, 1);
   auto answer_it = dfs_1_answer.begin();
   for (auto& heir : result) {
@@ -23,13 +23,32 @@ TEST_F(AlgorithmsTest, dfs_1) {
   }
 }
 
-TEST_F(AlgorithmsTest, bfs_1) {
+TEST_F(SearchAlgorithmsTest, bfs_1) {
   std::vector<int> result = s21::GraphAlgorithms::breadthFirstSearch(graph, 1);
   auto answer_it = bfs_1_answer.begin();
   for (auto& heir : result) {
       EXPECT_EQ(*answer_it, heir);
       answer_it++;
   }
+}
+
+struct PathAlgorithmsTest : public testing::Test {
+ protected:
+  s21::Graph graph;
+  void SetUp() {
+    graph.loadGraphFromFile("../datasets/sample6.txt");
+  }
+  void TearDown() {}
+};
+
+TEST_F(PathAlgorithmsTest, get_shortest_path_between_two_1) {
+  int result = s21::GraphAlgorithms::getShortestPathBetweenVertices(graph, 1, 2);
+  EXPECT_EQ(result, 10);
+}
+
+TEST_F(PathAlgorithmsTest, get_shortest_path_between_two_2) {
+  int result = s21::GraphAlgorithms::getShortestPathBetweenVertices(graph, 1, 7);
+  EXPECT_EQ(result, 30);
 }
 
 

ee26c757e5d1ae39175aec812a8899aa066213e3
rubiezzy <rubiezzy@student.21-school.ru>
2023-03-02 17:25:00 +0400

    added comments

--- src/algorithms_class/graph_algorithms.cc
+++ src/algorithms_class/graph_algorithms.cc
@@ -3,13 +3,28 @@
 #include "../helpers/containers/s21_queue.h"
 #include "../helpers/containers/s21_stack.h"
 
+/**
+ * Method for traversing graph data structure.
+ * The algorithm starts at the root node (selecting some arbitrary node
+ * as the root node in the case of a graph) and explores as far as possible
+ * along each branch before backtracking.
+ * @returns vector containing the traversed vertices in the order
+ * of their traversal
+*/
 std::vector<int> s21::GraphAlgorithms::depthFirstSearch(Graph& graph,
                                                         int start_vertex) {
   s21::Stack<int> not_visited_verticles;
   return searchAlgorithm_<s21::Stack<int>>(graph, start_vertex,
                                            not_visited_verticles);
 }
-
+/** 
+ * Method for traversing graph data structure.
+ * The algorithm starts at the tree’s root or graph and searches/visits 
+ * all nodes at the current depth level before moving on to the nodes at
+ * the next depth level.
+ * @returns vector containing the traversed vertices in the order
+ * of their traversal
+*/
 std::vector<int> s21::GraphAlgorithms::breadthFirstSearch(Graph& graph,
                                                           int start_vertex) {
   s21::Queue<int> not_visited_verticles;
@@ -17,15 +32,18 @@ std::vector<int> s21::GraphAlgorithms::breadthFirstSearch(Graph& graph,
                                            not_visited_verticles);
 }
 
+/**
+ * Method for finding the shortest path between vertex1
+ * and vertex2 in a weighted graph using Dijkstra's algorithm.
+ * @returns numerical result equal to the smallest distance
+*/
 int s21::GraphAlgorithms::getShortestPathBetweenVertices(Graph& graph,
                                                          int vertex1,
                                                          int vertex2) {
   vertex1 -= 1;
   std::map<int, bool> visited_vertices;
   s21::Queue<int> not_visited_vertices = {vertex1};
-
   int vertices_number = graph.getVerticesCount();
-
   std::vector<int> tags(vertices_number, std::numeric_limits<int>::max());
   tags[vertex1] = 0;
 
@@ -45,22 +63,28 @@ int s21::GraphAlgorithms::getShortestPathBetweenVertices(Graph& graph,
   return tags[vertex2 - 1];
 }
 
+/**
+ * Method for finding shortest paths between all pairs of
+ * vertices in a directed weighted graph using Floyd–Warshall algorithm.
+ * @returns matrix of shortest paths between all vertices of the graph.
+*/
 s21::Matrix<int> s21::GraphAlgorithms::getShortestPathsBetweenAllVertices(Graph& graph) {
     std::vector<Graph> graphs(graph.getVerticesCount() + 1, s21::Graph(graph.getVerticesCount()));
-    prepareGraphForFloydWarshallAlgo_(graph);
-    graphs.at(0) = graph;
-    for (int k = 1; k <= graph.getVerticesCount(); ++k) {
-        for (int i = 0; i < graph.getVerticesCount(); ++i) {
-            for (int j = 0; j < graph.getVerticesCount(); ++j) {
+    s21::Graph prepared_graph(graph);
+    prepareGraphForFloydWarshallAlgo_(prepared_graph);
+    graphs.at(0) = prepared_graph;
+    for (int k = 1; k <= prepared_graph.getVerticesCount(); ++k) {
+        for (int i = 0; i < prepared_graph.getVerticesCount(); ++i) {
+            for (int j = 0; j < prepared_graph.getVerticesCount(); ++j) {
                 Graph& prev_graph = graphs.at(k - 1);
                 graphs.at(k).getWeigth(i, j) =
                     std::min(
                         prev_graph.getWeigth(i, j),
-                        prev_graph.getWeigth(i, k-1) + prev_graph.getWeigth(k-1, j));
+                        prev_graph.getWeigth(i, k - 1) + prev_graph.getWeigth(k - 1, j));
             }
         }
     }
-    return graphs.at(graph.getVerticesCount()).graphToMatrix();
+    return graphs.at(prepared_graph.getVerticesCount()).graphToMatrix();
 }
 
 template <class Container>
@@ -105,7 +129,7 @@ void s21::GraphAlgorithms::prepareGraphForFloydWarshallAlgo_(Graph& graph) {
                 graph.getWeigth(i, j) = 0;
             } else {
                 if (graph.getWeigth(i, j) == 0) {
-                    graph.getWeigth(i, j) = 1000;
+                    graph.getWeigth(i, j) = 1000;  // здесь должен быть inf
                 }
             }
         }

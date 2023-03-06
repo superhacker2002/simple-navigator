76748631ddf0de872b5a6cfba9bbfb072f1f8f7e
rubiezzy <rubiezzy@student.21-school.ru>
2023-03-06 19:04:41 +0400

    removed excess

--- src/algorithms_class/ant_algorithms/tests.cc
+++
@@ -1,8 +0,0 @@
-#include "simple_aco.h"
-
-int main() {
-    s21::Graph graph;
-    graph.loadGraphFromFile("../../../datasets/sample9.txt");
-    SimpleACO path(graph);
-    auto result = path.findBestPath();
-}
